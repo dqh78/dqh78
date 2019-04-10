22d9de5da7c78f73a26188c1aa73a2d0484f8b68
dqh <2472537421@qq.com>
2019-04-10 16:51:05 +0800

    first commit

--- 12/src/org/circle/Area.java
+++ 12/src/org/circle/Area.java
@@ -1,5 +1,18 @@
-package org.circle;
-
+package org.circle; 
+/**
+ * Title:求圆的面积
+ * Description:已知圆的半径r，求圆的面积 * 
+ * @author 戴清海*
+ */
 public class Area {
+	public static void main(String[] args) {
+    final double PI=3.1416;
+    double r,area;
+    r=3;
+    area=PI*r*r;
+    System.out.println("圆的面积="+area);
+    
+	}   
 
 }
+
